89814b2a1039ae5abd75ce5f8deadc312b9ed2fa
Alberto Tostado <alberto.tostado@atmira.com>
2018-07-24 14:58:34 +0200

    auto diff v2

--- src/main/java/netgloo/models/Cabessa.java
+++ src/main/java/netgloo/models/Cabessa.java
@@ -7,57 +7,65 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
+@Entity
+@Table(name = "cabessas")
+public class Cabessa {
 
-	
-	@Entity
-	@Table(name = "cabessas")
-	public class Cabessa {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
 
-	  @Id
-	  @GeneratedValue(strategy = GenerationType.AUTO)
-	  private long id;
-	  
-	  @NotNull
-	  private String email;
-	   
-	  @NotNull
-	  private String name;
-	  
-	  public Cabessa() {}
+	@NotNull
+	private String email;
 
-	  
-	  public Cabessa(long id, @NotNull String email, @NotNull String name) {
+	@NotNull
+	private String name;
+
+	@NotNull
+	private boolean esCabeson;
+
+	public Cabessa() {
+	}
+
+	public Cabessa(long id, @NotNull String email, @NotNull String name, @NotNull boolean esCabeson) {
 		super();
 		this.id = id;
 		this.email = email;
 		this.name = name;
+		this.esCabeson = esCabeson;
 	}
 
 	// Getter and setter methods
-	  public long getId() {
-	    return id;
-	  }
+	public long getId() {
+		return id;
+	}
 
-	  public void setId(long value) {
-	    this.id = value;
-	  }
+	public void setId(long value) {
+		this.id = value;
+	}
 
-	  public String getEmail() {
-	    return email;
-	  }
-	  
-	  public void setEmail(String value) {
-	    this.email = value;
-	  }
-	  
-	  public String getName() {
-	    return name;
-	  }
+	public String getEmail() {
+		return email;
+	}
 
-	  public void setName(String value) {
-	    this.name = value;
-	  }
-	  
+	public void setEmail(String value) {
+		this.email = value;
+	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String value) {
+		this.name = value;
+	}
+
+	public boolean isEsCabeson() {
+		return esCabeson;
+	}
+
+	public void setEsCabeson(boolean esCabeson) {
+		this.esCabeson = esCabeson;
+	}
 
 }
